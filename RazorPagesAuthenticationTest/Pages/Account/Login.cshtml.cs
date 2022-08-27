f5ded7d35efe4569f6c62c94da26ab672bebeae7
piotryfon <31383071+piotryfon@users.noreply.github.com>
2022-08-27 07:37:17 +0200

    Add project files.

---
+++ RazorPagesAuthenticationTest/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesAuthenticationTest.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public Credential Credential { get; set; }
+        public void OnGet()//propaguje na front
+        {
+            //this.Credential = new Credential 
+            //{ 
+            //    UserName = "admin", 
+            //    Password = "1234" 
+            //};
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) 
+            {
+                return Page();
+            }
+
+            if (Credential.UserName == "admin" && Credential.Password == "1234")
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, "admin"),
+                    new Claim(ClaimTypes.Email, "admin@o2.pl")
+                };
+                var identity = new ClaimsIdentity(claims, "MyCookieAuth");
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
+
+                await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal); //serializuje na string, szyfruje i zapisuje jako cookie
+
+                TempData["Message"] = "Logged";
+
+                return RedirectToPage("/Index");
+            }
+            return Page();
+        }
+       
+    }
+    public class Credential
+    {
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
---
+++ RazorPagesAuthenticationTest/Pages/HumanResource.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesAuthenticationTest.Pages
+{
+    public class HumanResourceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
